5838de063f41ea58ecff0970f28fe253d60f77cd
Nathan McCarthy <nathanmccarthy13@gmail.com>
2021-06-21 21:14:46 -0500

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,7 +3,8 @@ import { View, Text, ScrollView } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { Flatlist } from 'react-native-gesture-handler';
 import { connect } from 'react-redux';
-import { baseUrl } from '../shared/baseUrl'; 
+import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
   return {
@@ -15,8 +16,7 @@ function Mission(props) {
     return (
       <Card title={'Our Mission'}>
         <Text>
-          We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of
-          resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have
+          We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have
           visited with each other.
         </Text>
       </Card>
@@ -39,6 +39,29 @@ class About extends Component {
               />
             );
         };
+
+        if (this.props.partners.isLoading) {
+          return (
+            <ScrollView>
+              <Mission />
+              <Card
+                title='Community Partners'>
+                <Loading />
+              </Card>
+            </ScrollView>
+          );
+        }
+        if (this.props.partners.errMess) {
+          return (
+            <ScrollView>
+              <Mission />
+              <Card
+                  title='Community Partners'>
+                  <Text>{this.props.partners.errMess}</Text>
+              </Card>
+            </ScrollView>
+          );
+        }
 
         return (
             <ScrollView>
