872cf562d6ef139efecebefc6cdb4b3524cffabc
Nathan McCarthy <nathanmccarthy13@gmail.com>
2021-07-08 17:03:34 -0500

    Week 4 Task 2

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -165,6 +165,22 @@ class RegisterTab extends Component {
         }
     }
 
+    getImageFromGallery = async () => {
+        const cameraRollPermission = await Permissions.askAsync(
+            Permissions.CAMERA_ROLL
+        );
+        if (cameraRollPermission.status === "granted") {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+              allowsEditing: true,
+              aspect: [1,1]
+            });
+            if(!capturedImage.cancelled) {
+              console.log(capturedImage);
+              this.processImage(capturedImage.uri);
+            }
+        }
+    }
+
     processImage = async (imgUri) => {
         const processedImage = await ImageManipulator.manipulateAsync(
             imgUri,
@@ -201,6 +217,10 @@ class RegisterTab extends Component {
                         <Button
                             title='Camera'
                             onPress={this.getImageFromCamera}
+                        />
+                        <Button 
+                            title='Gallery'
+                            onPress={this.getImageFromGallery}
                         />
                     </View>
                     <Input
