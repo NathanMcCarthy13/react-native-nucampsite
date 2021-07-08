38974cb047b8979620866fe540f20354cf3cea70
Nathan McCarthy <nathanmccarthy13@gmail.com>
2021-07-08 17:14:28 -0500

    Week 4 Task 3

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -321,17 +321,23 @@ const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
 
+    showNetInfo = async() => {
+        let connectionInfo = await NetInfo.fetch();
+        if (connectionInfo.ok) {
+            (Platform.OS === 'ios')
+                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+                : ToastAndroid.show('Initial Network Connectivity Type: ' + connectionInfo.type, ToastAndroid.LONG);
+            
+        }
+    }
+
     componentDidMount() {
         this.props.fetchCampsites();
         this.props.fetchComments();
         this.props.fetchPromotions();
         this.props.fetchPartners();
-
-        NetInfo.fetch().then(connectionInfo => {
-            (Platform.OS === 'ios')
-                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-                : ToastAndroid.show('Initial Network Connectivity Type: ' + connectionInfo.type, ToastAndroid.LONG);
-        });
+    
+        this.showNetinfo();
 
         this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
             this.handleConnectivityChange(connectionInfo);
